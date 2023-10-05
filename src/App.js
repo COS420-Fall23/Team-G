4f1fe1ab6e26f4bd87cb19c4229eb4971c512aa5
durga <durgamabhilash@gmail.com>
2023-10-04 23:40:25 -0400

    Created react App

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>BearPool</code> under development
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/COS420-Fall23/Team-G"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go to repository
         </a>
       </header>
     </div>
