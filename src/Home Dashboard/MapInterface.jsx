cd057c3764c9474b92eda349ec29546d77d9a7e5
durga <durgamabhilash@gmail.com>
2023-11-13 22:18:06 -0500

    Corrected build warnings

--- src/Home Dashboard/MapInterface.jsx
+++ src/Home Dashboard/MapInterface.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const center = [44.9019,-68.6688];
