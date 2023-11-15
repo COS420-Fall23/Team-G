a89ea7324db5b44ae630ef9f73a9d55e6378bc83
David Neufeld <20neufeldd@gmail.com>
2023-11-15 10:17:46 -0500

    Seperated ProfilePage formatting into .css

--- src/Home Dashboard/ProfilePage.jsx
+++ src/Home Dashboard/ProfilePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Review from './Review.jsx'
+import './ProfilePage.css';
 const UserInformation = {
   name: 'John Doe',
   major: 'Computer Science',
@@ -13,54 +14,49 @@ const UserInformation = {
 };
 
 const ProfilePage = () => {
-  //this.UserInformation=UserInformation
   return (
-    <div style={{ fontFamily: 'Arial', padding: '20px', backgroundColor: '#f5f5f5', maxWidth: '400px', margin: 'auto' }}>
-      
-      <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '20px' }}>
-        <h1 style={{ margin: 0 }}>Profile</h1>
-        <button style={{ backgroundColor: '#ddd', border: 'none', borderRadius: '5px', padding: '10px' }}>Edit Profile</button>
+    <div className="profile-page">
+      <header className="header">
+        <h1>Profile</h1>
+        <button className="edit-profile-button">Edit Profile</button>
       </header>
       
-      <section style={{ backgroundColor: '#fff', borderRadius: '5px', padding: '15px', marginBottom: '20px' }}>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '15px' }}>
-          <div style={{ width: '80px', height: '80px', borderRadius: '50%', backgroundColor: '#9e9e9e', marginBottom: '10px' }}></div>
-          <h2 style={{ margin: '0' }}>{UserInformation.name}</h2>
-          <p> <strong>Major:</strong> {UserInformation.major}</p>
+      <section className="user-section">
+        <div className="user-info">
+          <div className="user-image"></div>
+          <h2>{UserInformation.name}</h2>
+          <p><strong>Major:</strong> {UserInformation.major}</p>
           <p><strong>About Me:</strong> {UserInformation.aboutMeText}</p>
           <p><strong>Carpool Preferences:</strong> {UserInformation.preferences}</p>
         </div>
       </section>
 
-      <section style={{ backgroundColor: '#fff', borderRadius: '5px', padding: '15px' }}>
-        <div style={{ borderBottom: '1px solid #ddd', paddingBottom: '10px', marginBottom: '10px' }}>
+      <section className="reviews-section">
+        <div className="reviews-header">
           <h3>Reviews</h3>
           {UserInformation.reviews.length > 0 ? (
-            <h3>Carpool Preferences</h3>,
-            <div>
+            <div className="reviews-container">
               {UserInformation.reviews.map((review, index) => (
-                /* ToDo: Edit so that each of these draws the review component */
-                <Review key = {index} name = {review.name} rating = {review.rating} reviewText={review.text} /> //Pass props to the review component
+                <Review key={index} name={review.name} rating={review.rating} reviewText={review.text} />
               ))}
-              </div>
+            </div>
           ) : (
-            <div style={{ textAlign: 'center', padding: '20px', backgroundColor: '#e3f2fd', borderRadius: '5px' }}>
-            No Reviews Yet
+            <div className="no-reviews-container">
+              No Reviews Yet
             </div>
           )}
-          <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-            <button style={{ backgroundColor: '#ddd', border: 'none', borderRadius: '5px', padding: '10px' }}>Passenger</button>
-            <button style={{ backgroundColor: '#ddd', border: 'none', borderRadius: '5px', padding: '10px' }}>Driver</button>
+          <div className="carpool-preference-buttons">
+            <button className="passenger-button">Passenger</button>
+            <button className="driver-button">Driver</button>
           </div>
         </div>
-        
       </section>
 
-      <nav style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', marginTop: '30px' }}>
-        <button style={{ backgroundColor: '#ddd', border: 'none', borderRadius: '5px', padding: '10px' }}>Set Up Drive</button>
-        <button style={{ backgroundColor: '#ddd', border: 'none', borderRadius: '5px', padding: '10px' }}>Request Ride</button>
-        <button style={{ backgroundColor: '#ddd', border: 'none', borderRadius: '5px', padding: '10px' }}>Profile</button>
-        <button style={{ backgroundColor: '#ddd', border: 'none', borderRadius: '5px', padding: '10px' }}>Notifications</button>
+      <nav className="navigation">
+        <button className="set-up-drive-button">Set Up Drive</button>
+        <button className="request-ride-button">Request Ride</button>
+        <button className="profile-button">Profile</button>
+        <button className="notifications-button">Notifications</button>
       </nav>
     </div>
   );
