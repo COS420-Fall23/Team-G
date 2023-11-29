1c17d23d01ace05e8c46c756b3f3b8c066cfb53e
Quinn Colette Knowles <Quinn.knowles@maine.edu>
2023-11-29 13:14:49 -0500

    added a dismiss button

--- src/Home Dashboard/NotificationComponent.jsx
+++ src/Home Dashboard/NotificationComponent.jsx
@@ -1,16 +1,30 @@
 // NotificationComponent.jsx
 
-import React from 'react';
-import './NotificationComponent.css'; // Import the CSS file for styling
+import React, { useState } from 'react';
+import './NotificationComponent.css';
 
-const NotificationComponent = ({ notification }) => {
+const NotificationComponent = ({ notification, onDismiss }) => {
+  const [dismissed, setDismissed] = useState(false);
+
+  const handleDismiss = () => {
+    setDismissed(true);
+    if (onDismiss) {
+      onDismiss(notification.id); // Pass the notification id to the parent component for dismissal
+    }
+  };
+
   return (
-    <div className="notification-container">
-      <div className="notification">
-        <h3>{notification.title}</h3>
-        <p>{notification.message}</p>
+    !dismissed && (
+      <div className="notification-container">
+        <div className="notification">
+          <button className="dismiss-button" onClick={handleDismiss}>
+            Dismiss
+          </button>
+          <h3>{notification.title}</h3>
+          <p>{notification.message}</p>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
