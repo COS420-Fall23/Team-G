a9ce63c4389ab98a564659eb5c40e5b44957abd2
Quinn Colette Knowles <Quinn.knowles@maine.edu>
2023-11-29 19:38:36 -0500

    commit

--- src/Home Dashboard/NotificationComponent.test.jsx
+++ src/Home Dashboard/NotificationComponent.test.jsx
@@ -1,22 +1,50 @@
 // NotificationComponent.test.js
 
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import NotificationComponent from './NotificationComponent';
 
 describe('NotificationComponent', () => {
-  const notificationData = {
+  const notificationWithOptionalFeature = {
+    id: 1,
     title: 'Test Title',
     message: 'Test Message',
+    hasOptionalFeature: true,
   };
 
-  it('renders notification component with provided data', () => {
-    const { getByText } = render(<NotificationComponent notification={notificationData} />);
-    
-    const titleElement = getByText(notificationData.title);
-    const messageElement = getByText(notificationData.message);
+  const notificationWithoutOptionalFeature = {
+    id: 2,
+    title: 'Test Title 2',
+    message: 'Test Message 2',
+    hasOptionalFeature: false,
+  };
 
-    expect(titleElement).toBeInTheDocument();
-    expect(messageElement).toBeInTheDocument();
+  it('renders notification component with optional feature when hasOptionalFeature is true', () => {
+    const { getByText } = render(
+      <NotificationComponent notification={notificationWithOptionalFeature} />
+    );
+
+    const acceptButton = getByText('Accept');
+    const declineButton = getByText('Decline');
+
+    expect(acceptButton).toBeInTheDocument();
+    expect(declineButton).toBeInTheDocument();
   });
+
+  it('renders notification component without optional feature when hasOptionalFeature is false', () => {
+    const { getByText, queryByText } = render(
+      <NotificationComponent notification={notificationWithoutOptionalFeature} />
+    );
+
+    const acceptButton = queryByText('Accept');
+    const declineButton = queryByText('Decline');
+
+    expect(acceptButton).toBeNull();
+    expect(declineButton).toBeNull();
+
+    const dismissButton = getByText('Dismiss');
+    expect(dismissButton).toBeInTheDocument();
+  });
+
+  // Add more test cases for other functionalities as needed
 });
