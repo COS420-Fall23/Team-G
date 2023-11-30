4963f253b63292575a44f0d197439b45ab1d966b
David Neufeld <20neufeldd@gmail.com>
2023-11-29 20:01:44 -0500

    Implemented TimeSlotSelector

--- src/Home Dashboard/PreferencesPage.jsx
+++ src/Home Dashboard/PreferencesPage.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './PeferencesPage.css';
+import TimeSlotSelector from './TimeSlotSelector';
+import Navbar from './Navbar';
 
 const PreferencesPage = () => {
   const [smoking, setSmoking] = useState('');
@@ -79,20 +81,8 @@ const PreferencesPage = () => {
       
       {/* Available Times */}
       
-      <div>
-        <h2>Select Available times:</h2>
-        <button onClick={() => handleTimeSelection('8:00 AM')}>8 AM</button>
-        <button onClick={() => handleTimeSelection('9:00 AM')}>9 AM</button>
-        <button onClick={() => handleTimeSelection('10:00 AM')}>10 AM</button>
-        <button onClick={() => handleTimeSelection('11:00 AM')}>11 AM</button>
-        <button onClick={() => handleTimeSelection('12:00 PM')}>12 PM</button>
-        <button onClick={() => handleTimeSelection('1:00 PM')}>1 PM</button>
-        <button onClick={() => handleTimeSelection('2:00 PM')}>2 PM</button>
-        <button onClick={() => handleTimeSelection('3:00 PM')}>3 PM</button>
-        <button onClick={() => handleTimeSelection('4:00 PM')}>4 PM</button>
-        <button onClick={() => handleTimeSelection('5:00 PM')}>5 PM</button>
-        <button onClick={() => handleTimeSelection('6:00 PM')}>6 PM</button>
-      </div>
+      <TimeSlotSelector/>
+      <Navbar/>
 
       {/* Selected Times */}
       
