f185f066885f210e37451b32cc069139bb1216f1
Quinn Colette Knowles <Quinn.knowles@maine.edu>
2023-12-05 14:47:08 -0500

    fix unused variables

--- src/Home Dashboard/Notifications.jsx
+++ src/Home Dashboard/Notifications.jsx
@@ -14,85 +14,38 @@ import {
   Toolbar,
   Box,
   AppBar,
-  IconButton,
   Typography,
-  useTheme,
-  useMediaQuery,
 } from "@mui/material";
 import NotificationComponent from './NotificationComponent';
-import MenuIcon from '@mui/icons-material/Menu';
-import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
-import PeopleIcon from "@mui/icons-material/People";
-import MapIcon from "@mui/icons-material/Map";
 import Navbar from './Navbar.jsx'
-
-const drawerWidth = 240;
+import notificationsData from './notificationsData.js'; // Importing notifications from the separate file
+//import { db } from '../firebaseConfig.js';
+//import { doc, getDoc, updateDoc } from "firebase/firestore";
 
 const Notifications = () => {
-  const navigate = useNavigate();
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const uid = "user1" //todo: set based on logged in user
+  const userNotifications = notificationsData.userNotifications[uid] || [];
 
-  const handleBackButtonClick = () => {
-    navigate(-1);
-  };
-  const handleProfile = (event) => {
-    event.preventDefault();
-    navigate("/ProfilePage");
-  };
-  const handleNotifications = (event) => {
-    event.preventDefault();
-    navigate("/Notifications");
-  };
-  const [mobileOpen, setMobileOpen] = useState(false); // State to handle drawer for mobile view
 
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-  const notifications = [
-    {
-      id: 1,
-      title: 'Notification 1',
-      message: 'This is the first notification.',
-      hasOptionalFeature: false, // Set hasOptionalFeature for each notification as needed
-    },
-    {
-      id: 2,
-      title: 'Notification 2',
-      message: 'This is the second notification.',
-      hasOptionalFeature: false,
-    },
-    {
-      id: 3,
-      title: 'Notification 3',
-      message: 'This is the third notification.',
-      hasOptionalFeature: true, // Third notification has the optional feature
-    },
-  ];
   return (
     <Box>
-    <AppBar position="fixed">
-      <Toolbar>
-        <Typography variant="h6" noWrap component="div">
-          Notifications
-        </Typography>
-      </Toolbar>
-    </AppBar>
-        <div className="app">
-      <div className="notification-list">
-        {notifications.map((notification) => (
-          <NotificationComponent key={notification.id} notification={notification} />
-        ))}
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Notifications
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div className="app">
+        <div className="notification-list">
+          {userNotifications.map((notification) => (
+            <NotificationComponent key={notification.id} notification={notification} />
+          ))}
+        </div>
       </div>
-    </div>
-    <Navbar />
-      </Box>
-      
-      
+      <Navbar />
+    </Box>
   );
-  
-  
-
 };
 
 export default Notifications;
